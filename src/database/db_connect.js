236f8239e80707b58bb50ccab30e8239a858e483
Piotr Berebecki <petebere@gmail.com>
2017-04-05 16:25:51 +0100

    Set max connections to 20

--- src/database/db_connect.js
+++ src/database/db_connect.js
@@ -13,7 +13,7 @@ const options = {
   host: params.hostname,
   port: params.port,
   database: params.pathname.split('/')[1],
-  max: process.env.DB_MAX_CONNECTIONS || 2,
+  max: process.env.DB_MAX_CONNECTIONS || 20,
   ssl: (params.hostname !== 'localhost'),
 }
 
