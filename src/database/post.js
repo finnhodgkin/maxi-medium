08ad03b7d457d6d95134cec01c862216df691240
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-04-06 11:40:15 +0100

    started basic sql queries relates #27

--- src/database/post.js
+++ src/database/post.js
@@ -1,1 +1,6 @@
 const connect = require('./db_connect');
+
+function post(err, object) {
+  if (err) return err;
+  connect.query(`INSERT INTO articles (author_id, title, body_text, image_url) VALUES ('${}', '${object.title}', ${object.body_text}, '${object.image_url}')`);
+}
