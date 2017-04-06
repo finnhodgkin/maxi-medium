54ccfd411b9e403054b436d6def366b32a7027ae
bo-bok <bo-bok@users.noreply.github.com>
2017-04-06 13:07:00 +0100

    make post data an exportable object called post, partial implementation of sending post data to db
    relates #27

--- src/database/post.js
+++ src/database/post.js
@@ -1,6 +1,11 @@
 const connect = require('./db_connect');
 
-function post(err, object) {
+const post = {};
+
+post.articles = (err, object) => {
   if (err) return err;
-  connect.query(`INSERT INTO articles (author_id, title, body_text, image_url) VALUES ('${}', '${object.title}', ${object.body_text}, '${object.image_url}')`);
+  connect.query(`INSERT INTO articles (title, body_text, image_url)
+                 VALUES ( '${object.title}', ${object.body_text}, '${object.image_url}')`);
 }
+
+module.exports = post;
--- src/routes/submit.js
+++ src/routes/submit.js
@@ -1,8 +1,17 @@
+const post = require('./../database/post');
+
 module.exports = {
   method: 'POST',
   path: '/submit',
   handler: (req, reply) => {
+    // this is what's breaking conole the req.payload to the terminal
+    // post.articles((err, payload) => {
+    //   if (err) {
+    //     console.log(err);
+    //     return;
+    //   }
     console.log(req.payload);
-    reply('hi');
+    reply('new_article');
+    // })
   }
 }
