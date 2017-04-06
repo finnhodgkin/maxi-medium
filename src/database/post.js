f7a6eafb016397b2240cb273b86266b51c4b3a9a
bo-bok <bo-bok@users.noreply.github.com>
2017-04-06 15:01:24 +0100

    hooks up post data to db and redirects to homepage on form submit
    relates #27

--- src/database/post.js
+++ src/database/post.js
@@ -2,10 +2,14 @@ const connect = require('./db_connect');
 
 const post = {};
 
-post.articles = (err, object) => {
-  if (err) return err;
-  connect.query(`INSERT INTO articles (title, body_text, image_url)
-                 VALUES ( '${object.title}', ${object.body_text}, '${object.image_url}')`);
+post.articles = (newArticle, callback) => {
+  connect.query(`INSERT INTO articles (author_id, title, body_text, image_url)
+                 VALUES (1, '${newArticle.title}', '${newArticle.body_text}', '${newArticle.image_url}') RETURNING id`, (err, res) => {
+
+                   if (err) return callback(err);
+
+                   callback(null, res)
+                 });
 }
 
 module.exports = post;
--- src/routes/submit.js
+++ src/routes/submit.js
@@ -4,14 +4,13 @@ module.exports = {
   method: 'POST',
   path: '/submit',
   handler: (req, reply) => {
-    // this is what's breaking conole the req.payload to the terminal
-    // post.articles((err, payload) => {
-    //   if (err) {
-    //     console.log(err);
-    //     return;
-    //   }
-    console.log(req.payload);
-    reply('new_article');
-    // })
+
+    post.articles(req.payload, (err, id) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      reply.redirect('/');
+    })
   }
 }
