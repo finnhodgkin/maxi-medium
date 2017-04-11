ab0fd986acca1b73ec3977cff41e7d3f4c7b423f
PiotrBerebecki <petebere@gmail.com>
2017-04-11 22:29:01 +0100

    Modify build script to add more user fields
    relates #4

--- database_build/db_build.sql
+++ database_build/db_build.sql
@@ -4,29 +4,31 @@ DROP TABLE IF EXISTS users, articles CASCADE;
 
 CREATE TABLE users (
   id          SERIAL        PRIMARY KEY,
-  username    VARCHAR(64)   UNIQUE NOT NULL,
-  password    VARCHAR(64)  NOT NULL,
-  avatar_url  VARCHAR(500)
+  username    VARCHAR(64)   UNIQUE,
+  first_name  VARCHAR(64)   NOT NULL,
+  last_name   VARCHAR(64)   NOT NULL,
+  password    VARCHAR(64)   NOT NULL,
+  avatar_url  VARCHAR(500)  NOT NULL
 );
 
 CREATE TABLE articles (
-  id          SERIAL          PRIMARY KEY,
-  author_id   INTEGER         REFERENCES users(id),
-  title       VARCHAR(100)    NOT NULL,
-  body_text   VARCHAR(20000)  NOT NULL,
-  image_url   VARCHAR(500),
-  date_posted VARCHAR(50)
+  id           SERIAL          PRIMARY KEY,
+  author_id    INTEGER         REFERENCES users(id),
+  title        VARCHAR(100)    NOT NULL,
+  body_text    VARCHAR(20000)  NOT NULL,
+  image_url    VARCHAR(500)    NOT NULL,
+  date_posted  VARCHAR(50)     NOT NULL
 );
 
-INSERT INTO users (username, password, avatar_url)
-VALUES ('u1', '$2a$10$z.hMdUW5o1U.W95Vg/u.GOb65q6fRtsQY6mlykvbMZEHST2dASwz.', './images/jessica_headshot.jpg'),
-       ('u2', '$2a$10$6.m3YwrAjslzM2nUnwOMG.CSm2aVAfJrJxJOdU19eORVOHqz4UwDW', './images/finn_headshot.jpg');
+INSERT INTO users (username, first_name, last_name, password, avatar_url)
+VALUES
+('u1', 'Finn', 'Hodgkin', '$2a$10$lbczBZ5YwKsO6eC2Tm.9/.Xtky/2qjoXVR607Zs0ejmriJengZFqS', './images/finn_headshot.jpg'),
+('u2', 'Piotr', 'Berebecki', '$2a$10$XP3ZfjT33e0Qf.ANb8bJGegtVfhv1V7P2xsCry4I8W8p68.hBt/i2', './images/piotr_headshot.jpg');
 
 INSERT INTO articles (author_id, title, body_text, image_url, date_posted)
-VALUES (1, 'how to feed your fish', 'Aenean lacinia bibendum nulla sed consectetur.
-
+VALUES
+(1, 'I really like the outdoors', 'Aenean lacinia bibendum nulla sed consectetur.
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur blandit tempus porttitor.', 'https://images.unsplash.com/photo-1430692032771-aae00435a3ff?dpr=1&auto=format&fit=crop&w=1080&h=NaN&q=80&cs=tinysrgb&crop=&bg=', 1491476523775),
-
-        (2, 'I really like the outdoors', 'Aenean lacinia bibendum nulla sed consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur blandit tempus porttitor.', 'https://images.unsplash.com/photo-1477777404980-710a373f2d7e?dpr=1&auto=format&fit=crop&w=1080&h=NaN&q=80&cs=tinysrgb&crop=&bg=', 1491562292752);
+(2, 'I love to create', 'Aenean lacinia bibendum nulla sed consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur blandit tempus porttitor.', 'https://images.unsplash.com/photo-1477777404980-710a373f2d7e?dpr=1&auto=format&fit=crop&w=1080&h=NaN&q=80&cs=tinysrgb&crop=&bg=', 1491562292752);
 
 COMMIT;
